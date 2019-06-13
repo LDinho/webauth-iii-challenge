330cce2cd7d289b70f04a20730a1c75b04a87ad4
LD <LDinho@users.noreply.github.com>
2019-06-13 03:22:53 -0400

    add userRoute & user db helper functions

--- api/routes/users/usersRouter.js
+++ api/routes/users/usersRouter.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+  getUsers,
+
+} = require('../../helpers');
+
 
 /*
 @GET: users
@@ -9,8 +14,23 @@ const router = express.Router();
 */
 
 router.get('/', async (req, res) => {
+  try {
+    const users = await getUsers();
 
+    if (users.length) {
+      res.status(200).json(users)
 
+    } else {
+      res.status(404).json({message: `No users found`})
+
+    }
+  }
+  catch (err) {
+    res.status(500).json({
+      err: err.message,
+      error: `Unable to retrieve users`
+    })
+  }
 });
 
 module.exports = router;
