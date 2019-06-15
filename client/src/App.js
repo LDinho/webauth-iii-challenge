be60f783bf7a446250665981c77baa232e4dcb17
LD <LDinho@users.noreply.github.com>
2019-06-14 22:00:04 -0400

    add home component & minor styling
    -add exact path to home

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
-import SignUp from './Signup'
-import Login from './Login'
-import Users from './Users'
+
+import SignUp from './Signup';
+import Login from './Login';
+import Users from './Users';
+import Home from './Home';
 
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Auth User App
+        </h1>
         <nav>
           <ul>
+            <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/login">Log in</NavLink></li>
             <li><NavLink to="/signup">Sign up</NavLink></li>
             <li><NavLink to="/users">Users</NavLink></li>
           </ul>
         </nav>
-        <h1>
-          Auth User App
-        </h1>
       </header>
       <main>
+        <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
         <Route path="/users" component={Users} />
